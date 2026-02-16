2b221c0f19e63579baba258a7310e9b5d10083c5
Afshin.Amini <mnafshin@gmail.com>
2026-02-16 01:31:45 +0100

    init commit

---
+++ src/main/java/info/mnafshin/locks_in_java/DemoLocksInJavaApplication.java
@@ -0,0 +1,13 @@
+package info.mnafshin.locks_in_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLocksInJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLocksInJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/mnafshin/locks_in_java/DemoLocksInJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package info.mnafshin.locks_in_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLocksInJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
